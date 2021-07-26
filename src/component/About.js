7f546783293976567a26d512ec4ffaa06bc08dba
shimesaba0218 <shimesaba0218@gmail.com>
2021-07-26 13:38:14 +0900

    学校のアイコンを使わなくなったので削除

--- src/component/About.js
+++ src/component/About.js
@@ -4,7 +4,6 @@ import Container from '@material-ui/core/Container';
 import {makeStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import CakeIcon from '@material-ui/icons/Cake';
-import SchoolIcon from '@material-ui/icons/School';
 import BusinessIcon from '@material-ui/icons/Business';
 import PersonIcon from '@material-ui/icons/Person';
 import FavoriteIcon from '@material-ui/icons/Favorite';
